778ae84af61f48899ad0c66249e3da381195e729
Eric Kuoch <ek.kuoch@gmail.com>
2024-07-21 17:11:21 +0200

    feat(user_serach): add scope to find by criteria

--- backend/app/models/user_search.rb
+++ backend/app/models/user_search.rb
@@ -1,3 +1,14 @@
 class UserSearch < ApplicationRecord
   belongs_to :user
+
+  scope :search_by_criteria, -> (criteria) { where(construct_criteria_query(criteria)) }
+  
+  private
+
+  def self.construct_criteria_query(criteria)
+    criteria.map do |key, value|
+      value.present? ? "search_criteria->>'#{key}' LIKE '#{value}'" : nil
+    end.compact.join(' OR ') 
+  end
+
 end

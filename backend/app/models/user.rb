1e32440559f36d9171ebe18e227ae12eb5c9ce8b
Eric Kuoch <ek.kuoch@gmail.com>
2024-07-21 17:09:18 +0200

    feat(user): add role expert to user

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum :role, [:user, :expert, :admin ]
          
   has_many :user_searches
+  has_many :tasting_notes
+  has_many :notifications
 end

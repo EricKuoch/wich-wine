88e2b7c4205754bf891201fb5659448169947a4e
Eric Kuoch <ek.kuoch@gmail.com>
2024-07-21 17:08:56 +0200

    feat(models): add tasting note validation (only user expert)

--- backend/app/models/tasting_note.rb
+++ backend/app/models/tasting_note.rb
@@ -2,4 +2,13 @@ class TastingNote < ApplicationRecord
   belongs_to :user
   belongs_to :wine
 
+  validate :user_must_be_expert 
+
+  private
+
+  def user_must_be_expert
+    unless user&.expert?
+      errors.add(:user, 'must be an expert to create a tasting note')
+    end
+  end
 end
